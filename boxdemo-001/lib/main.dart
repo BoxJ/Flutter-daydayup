b82a3520410cbb634ecb2e885fb1ddb2e9003ed1
jingliang <jingliang@fotoplace.cc>
2019-09-05 10:12:42 +0800

    day-001

--- boxdemo-001/lib/main.dart
+++ boxdemo-001/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'model/people.dart';
 
 //void main() {
 //  runApp(
@@ -6,41 +7,124 @@ import 'package:flutter/material.dart';
 //  );
 //}
 
+
 void main() => runApp(BoxApp());
 class BoxApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Box Demo'),
-        ),
-        body: BoxWidgetLess(),
-      ),
+      debugShowCheckedModeBanner: false, //隐藏右上角DEBUG标识
+      home: BoxHome(),
       theme: ThemeData(
-        primaryColor: Colors.purple
+          primaryColor: Colors.blue
       ),
     );
   }
 }
-
-
-
-class BoxWidgetLess extends StatelessWidget {
+class BoxHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-
-    final _boxTxtStyle = TextStyle(
-      color: Colors.blue,
-      fontSize: 36.0,
-      fontWeight: FontWeight.bold
-    );
-    return Center(
-        child: Text('一个阿狸',
-            textDirection: TextDirection.ltr,
-            style: _boxTxtStyle
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Box Demo'),
+      ),
+      body: ListView.builder(
+        itemCount: boxArr.length,
+        itemBuilder: _boxCellForRow,
+      ),
     );
   }
+// cellForRow方法
+//  横向 Row
+  Widget _boxCellForRow(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      height: 88.0,
+      margin: EdgeInsets.all(6.0),
+      child: Row(
+        children: <Widget>[
+          Image.network(boxArr[index].avatar),
+          SizedBox(width: 10),
+          Text(boxArr[index].name)
+        ],
+      ),
+    );
+  }
+////  竖向 Column
+//  Widget _boxCellForRow(BuildContext context, int index) {
+//    return Container(
+//      color: Colors.white,
+//      height: 240.0,
+//      margin: EdgeInsets.all(6.0),
+//      child: Column(
+//        children: <Widget>[
+//          Image.network(boxArr[index].avatar),
+//          Text(boxArr[index].name)
+//        ],
+//      ),
+//    );
+//  }
+////  垂直 z轴 Stack
+//  Widget _boxCellForRow(BuildContext context, int index) {
+//    return Container(
+//      color: Colors.white,
+//      height: 240.0,
+//      margin: EdgeInsets.all(6.0),
+//      child: Stack(
+//        children: <Widget>[
+//          Image.network(boxArr[index].avatar),
+//          Text(boxArr[index].name)
+//        ],
+//      ),
+//    );
+//  }
+////  直接一个图片控件
+//  Widget _boxCellForRow(BuildContext context, int index) {
+//    return Container(
+//      color: Colors.white,
+//      height: 88.0,
+//      margin: EdgeInsets.all(6.0),
+//      child: Image.network(boxArr[index].avatar),
+//    );
+//  }
+//  Widget _boxCellForRow(BuildContext context, int index) {
+//    return Text('阿狸', textDirection: TextDirection.ltr);
+//  }
 }
+
+
+//class BoxApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Box Demo'),
+//        ),
+//        body: BoxWidgetLess(),
+//      ),
+//      theme: ThemeData(
+//        primaryColor: Colors.purple
+//      ),
+//    );
+//  }
+//}
+
+//class BoxWidgetLess extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//
+//    final _boxTxtStyle = TextStyle(
+//      color: Colors.blue,
+//      fontSize: 36.0,
+//      fontWeight: FontWeight.bold
+//    );
+//    return Center(
+//        child: Text('一个阿狸',
+//            textDirection: TextDirection.ltr,
+//            style: _boxTxtStyle
+//        )
+//    );
+//  }
+//}
---
+++ boxdemo-001/lib/model/people.dart
@@ -0,0 +1,16 @@
+class People {
+//  构建方法
+  const People({
+    this.name,
+    this.avatar
+  });
+//  两个属性
+  final String name;
+  final String avatar;
+}
+// 这里声明一个公开的数据源
+final List<People> boxArr = [
+  People(name:'阿狸1',avatar: 'http://cache.fotoplace.cc/ap2/190521/0/7B5C54B95100DE281E689EEF24E499D9.png'),
+  People(name:'阿狸2',avatar: 'http://cache.fotoplace.cc/ap2/190521/0/6478737E09CA39170389191133953111.png'),
+  People(name:'阿狸3',avatar: 'http://cache.fotoplace.cc/ap2/190521/0/5FD190ECFF4943167F3D5F19A2973D6D.png')
+];
